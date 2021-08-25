c5a99b6be9b7b3b279b726888ea601208dc4ca5f
Lester Inga <lester11inga@gmail.com>
2021-08-25 11:49:17 -0500

    cambios

--- classes/fourthProject/pakiman.js
+++ classes/fourthProject/pakiman.js
@@ -1,5 +1,17 @@
-class pakiman {
-    constructor() {
-        
+class Pakiman {
+    constructor(n, v, a){
+        this.nombre = n;
+        this.vida = v;
+        this.ataque = a;
     }
-}
+    hablar(){
+        alert(this.nombre);
+    }
+} 
+
+let cauchin = new Pakiman('Cauchin', 100, 30);
+let pokacho = new Pakiman('Pokacho', 80, 50);
+let tocinauro = new Pakiman('Tocinauro', 120, 40);
+let zorrauro = new Pakiman('Zorrauro', 130, 50);
+
+console.log(cauchin, pokacho, tocinauro, zorrauro);
