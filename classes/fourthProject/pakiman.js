061e8b69e36c8b38bacec2182de732dd48cc202c
Lester Inga <lester11inga@gmail.com>
2021-08-25 12:31:45 -0500

    pakiman

--- classes/fourthProject/pakiman.js
+++ classes/fourthProject/pakiman.js
@@ -1,17 +1,33 @@
+let imagenes = [];
+imagenes['Cauchin'] = './assets/vaca.png';
+imagenes['Pokacho'] = './assets/pollo.png';
+imagenes['Tocinauro'] = './assets/cerdo.png';
+
 class Pakiman {
     constructor(n, v, a){
+        this.imagen = new Image();
         this.nombre = n;
         this.vida = v;
         this.ataque = a;
+        this.imagen.src = imagenes[this.nombre];
     }
     hablar(){
         alert(this.nombre);
+    }
+    mostrar(){
+        document.body.appendChild(this.imagen);
+        document.write('<p>');
+        document.write('<strong>' + this.nombre + '</strong></br>');
+        document.write('vida: ' + this.vida + '</br>');
+        document.write('ataque: ' + this.ataque + '<hr>');
+        document.write('</p>');
     }
 } 
 
 let cauchin = new Pakiman('Cauchin', 100, 30);
 let pokacho = new Pakiman('Pokacho', 80, 50);
 let tocinauro = new Pakiman('Tocinauro', 120, 40);
-let zorrauro = new Pakiman('Zorrauro', 130, 50);
 
-console.log(cauchin, pokacho, tocinauro, zorrauro);
+pokacho.mostrar();
+cauchin.mostrar();
+tocinauro.mostrar();
